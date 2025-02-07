50f12614641bc35fb83c054a8fd1b36c9b9cd69d
smranthu <alugristoy@educacion.navarra.es>
2025-02-07 08:28:44 +0100

    Update coche.py

--- gestion_reservas/models/coche.py
+++ gestion_reservas/models/coche.py
@@ -2,8 +2,11 @@ from odoo import models, fields
 
 class Coche(models.Model):
     _name = 'coche'
-    _description = 'Mi gestion de reservas'
+    _description = 'Coche Disponible para reserva'
 
-    name = fields.Char(string='Nombre', required=True)
-    description = fields.Text(string='Descripción')
-    active = fields.Boolean(string='Activo', default=True)
+    name = fields.Char(string='Modelo del Coche', required=True)
+    marca= fields.Char(string="Marca",required=True)
+    precio_por_dia=fields.Float(string="Precio por Dia", required=True)
+    disponible=fields.Boolean(string="Disponible",default=True)
+    matricula=fields.Char(string="matricula",required=True,unique=True)
+    
