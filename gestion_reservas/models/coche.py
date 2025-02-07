29b9d39206f75a57b98f68620e2597d8a05eca54
smranthu <alugristoy@educacion.navarra.es>
2025-02-07 08:41:23 +0100

    Completado Coche.py

--- gestion_reservas/models/coche.py
+++ gestion_reservas/models/coche.py
@@ -6,7 +6,21 @@ class Coche(models.Model):
 
     name = fields.Char(string='Modelo del Coche', required=True)
     marca= fields.Char(string="Marca",required=True)
-    precio_por_dia=fields.Float(string="Precio por Dia", required=True)
+    precio_por_dia = fields.Float(string='Precio por Día', required=True)
     disponible=fields.Boolean(string="Disponible",default=True)
     matricula=fields.Char(string="matricula",required=True,unique=True)
+    tipo_combustible=fields.Selection([
+        ("gasolina","Gasolina"),
+        ("diesel","Diesel"),
+        ("hibrido","Hibrido"),
+        ("electrico","Electrico"),
+    ],string="Tipo de combustible",required=True)
     
+    num_asientos=fields.Integer(String="Numero de asientos",default=5)
+    transmision=fields.Selection([
+        ("manual","Manual",)
+        ("automatica","Automatica",)
+    ], string="Transemision", required=True)
+    
+    kilometraje=fields.Float(string="Kilomentraje (km)")
+    color=fields.Char(string="Color")
