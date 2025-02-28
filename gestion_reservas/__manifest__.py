bfb3eebd97e7c9d4f0a0425c46c17d6bc7d9e584
smranthu <alugristoy@educacion.navarra.es>
2025-02-28 08:47:03 +0100

    seguridad

--- gestion_reservas/__manifest__.py
+++ gestion_reservas/__manifest__.py
@@ -5,7 +5,9 @@
     'author': 'Bugisoft S.A',
     'depends': ['base','account'],
     'data': [
+        'security/res_groups.xml',
         'security/ir.model.access.csv',
+        'security/rules.xml',
         'views/gestion_reservas_views.xml',
         'views/crear_coches_views.xml',
         'views/crear_clientes_views.xml',
